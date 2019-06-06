d36dfc4955f101aebf285a3eedb1406e80d3276d
Joshua Clark <josh.slark@gmail.com>
2019-06-05 22:05:00 -0500

    Implement "Generate Barcode" Button
    Correct the eventListener code for the button so that it works properly. It now generates a
    preview of the barcode based on prefix and sku provided and inserts it
    into the document.

--- client/index.js
+++ client/index.js
@@ -1,22 +1,21 @@
 /* global $, JsBarcode*/
 
-$(function insertBarcode() {
+function insertBarcode() {
 	function onFailure(error) {
 		console.error ("Printing: " + error);
 	}
-	JsBarcode( '#barcode' ).init();
 	var barcodeCanvas = document.querySelector("#barcode");
 	var base64Str = barcodeCanvas.toDataURL('image/png').replace(/^data:image\/png;base64,/, "");
 	google.script.run.withFailureHandler(onFailure).
 		appendImage(base64Str);
-});
+}
 
 function updateBarcodePreview() {
 	var prefix = $('input[name="barcodePrefix"]').val();
 	var sku = $('input[name="barcodeSku"]').val();
-
+	sku.replace("-","");
 	JsBarcode("#barcode", prefix+sku);
-	console.log(prefix+sku);
+	insertBarcode();
 }
 
-$("#barcodeGeneratorStart").on("click", updateBarcodePreview());
+document.querySelector('input[name="barcodeGeneratorStart"]').addEventListener("click", updateBarcodePreview);
